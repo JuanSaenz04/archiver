0dd3954cd38f5db830e8452d36f122f4736b235c
Juan Ignacio <27705456+JuanSaenz04@users.noreply.github.com>
2026-01-04 18:36:15 -0300

    feat: added reload button to archives list

--- front/src/components/app-sidebar.tsx
+++ front/src/components/app-sidebar.tsx
@@ -3,7 +3,9 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupAction,
   SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -11,7 +13,7 @@ import {
 } from "@/components/ui/sidebar"
 import { apiClient } from "@/lib/api"
 import type { Archive, GetArchivesResponse } from "@/models/archive"
-import { File } from "lucide-react"
+import { File, RefreshCw } from "lucide-react"
 import { useEffect, useState, type Dispatch, type SetStateAction } from "react"
 
 interface Props {
@@ -22,13 +24,17 @@ interface Props {
 export function AppSidebar({ onArchiveSelected, selectedArchive }: Props) {
 
   const [ archives, setArchives ] = useState<Archive[] | null >(null)
+  const [ loading, setLoading ] = useState(false)
 
   const fetchArchives = async () => {
+    setLoading(true)
     try {
       const data = await apiClient.get('/archives') as GetArchivesResponse;
       setArchives(data.archives);  
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -43,6 +49,10 @@ export function AppSidebar({ onArchiveSelected, selectedArchive }: Props) {
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
+          <SidebarGroupLabel>Archive List</SidebarGroupLabel>
+          <SidebarGroupAction onClick={fetchArchives} title="Refresh Archives">
+            <RefreshCw className={loading ? "animate-spin" : ""} />
+          </SidebarGroupAction>
           <SidebarGroupContent>
             <SidebarMenu>
               { archives ?
