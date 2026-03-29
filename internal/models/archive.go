3408be5bf8de6f1aca91821a2ad852eafb1d0922
Juan Ignacio <27705456+JuanSaenz04@users.noreply.github.com>
2026-03-29 11:06:17 -0300

    feat: added metadata fields to archive struct model

--- internal/models/archive.go
+++ internal/models/archive.go
@@ -1,5 +1,16 @@
 package models
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Archive struct {
-	Name string `json:"name"`
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	SourceURL   string    `json:"source_url"`
+	Tags        []string  `json:"tags"`
+	CreatedAt   time.Time `json:"created_at"`
 }
