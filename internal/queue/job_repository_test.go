89035b853e0a0f4ecf0bf8912bd8374c993d2949
Juan Ignacio <27705456+JuanSaenz04@users.noreply.github.com>
2026-06-19 22:07:55 -0300

    style(api): fixed formatting issue in job_repository_test.go

--- internal/queue/job_repository_test.go
+++ internal/queue/job_repository_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
+
 func TestJobRepository_GetAllJobs_Empty(t *testing.T) {
 	_, rdb, ctx := newTestRedis(t)
 	repo := NewJobRepository(rdb)
